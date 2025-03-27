428005f240af08bd495dda16f7346534d48e18ec
Isaiah Muchene <imuchene@msn.com>
2025-03-27 17:34:40 +0300

    Applied the knowledge from the vector store

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,45 @@
+import { createOrReadVectorStoreIndex } from "@/lib/vector-store";
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
+import { MetadataMode } from "llamaindex";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-  console.log('messages', messages);
+
+  const systemMessage = {
+    role: 'system',
+    content: 'You are a helpful AI assistant'
+  };
+
+  const latestMessage = messages[messages.length - 1 ];
+  const index = await createOrReadVectorStoreIndex();
+
+  const retriever = index.asRetriever();
+  retriever.similarityTopK = 1;
+
+  const [matchingNode] = await retriever.retrieve(latestMessage.content);
+
+  console.log('matchingNode', matchingNode);
+
+  if (matchingNode.score > 0.8) {
+    const knowledge = matchingNode.node.getContent(MetadataMode.NONE);
+
+    systemMessage.content = `
+      You are a helpful AI  assistant. Your knowledge is enriched by this document: 
+      ---
+      ${knowledge}
+      ---
+      When possible, explain the reasoning for your responses based on this knowledge.
+    `;
+
+    console.log('knowledge', knowledge)
+  }
+
 
   const response = streamText({
     model: openai('gpt-4o-mini'),
-    messages: messages
+    messages: messages,
+    system: systemMessage.content
   });
 
   return response.toDataStreamResponse();
